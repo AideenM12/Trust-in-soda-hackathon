6ebf5fa1aa50ab8b602ae8579836e50b136a8695
AideenM12 <a.malone1289@gmail.com>
2021-09-09 22:22:47 +0000

    Create flask app and link to mongodb

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+# Change to False before submission
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
