66ec80becb6ee44a19d8950fd00e744a49125904
Scott Böning <scott.boening@gmx.de>
2021-09-12 20:17:51 +0000

    refactor: remove whitespace, superflous empty lines, add comments

--- static/js/hideThemeButtons.js
+++ static/js/hideThemeButtons.js
@@ -1,3 +1,4 @@
+// needs to be added to every page except index.html because the buttons had to be added to base.html to be applied to every page
 const hideButtons = () => {
     $('#themeSelectors').addClass('d-none')
 }
