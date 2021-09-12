56e5ca5ea4fe3b4cd9015f50bbaa24f367ef373e
Scott Böning <scott.boening@gmx.de>
2021-09-12 09:26:47 +0000

    fix: adjust footer to match header and adjust color changing function

--- static/js/script.js
+++ static/js/script.js
@@ -2,22 +2,22 @@ const changeTheme = () => {
     $('body')
     .css('background-color', '#3a6596')
     .css('color', '#ffffff');
+    $('#Blue')
+    .removeClass('btn-primary')
+    .addClass('btn-light')
+    .css('color', ('#3a6596'));
     $('.btn-block').css('color', '#ffffff');
-    $('.footer-text').css('color', '#000');
-    $('.list-unstyled')
-    .removeClass("text-white")
-    .addClass('text-black');
 }
 
 const defaultTheme = () => {
     $('body')
     .css('background-color', '#ffffff')
     .css('color', '#000');
+    $('#Blue')
+    .removeClass('btn-light')
+    .addClass('btn-primary')
+    .css('color', ('#fff'));
     $('.btn-block').css('color', '#000');
-    $('.footer-text').css('color', '#fff');
-    $('.list-unstyled')
-    .removeClass('text-black')
-    .addClass('text-white');
 }
 
 const toggleBlueButton = document.getElementById('Blue');
